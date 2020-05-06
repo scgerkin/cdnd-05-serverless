32f2bb1a23e3eb433d6e8e5e05d945d0dfbddec0
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-06 11:48:43 -0400

    Basic get all functionality
    Set up for parsing ID from JWT, needs implementation after
    auth is set up.

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -6,21 +6,41 @@ import { parseUserId } from '../../auth/utils'
 
 const logger = createLogger('getAllTodos');
 
+// TODO pull auth validation logic into a service to reduce duplication
+// TODO Implement middleware for headers/error handling
+// TODO Parse ID from JWT to use
+// TODO Validate existing ID (consider extracting to a service)
 
-// TODO: Get all TODO items for a current user
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
-  logger.info("Received event", event);
+  const authorization = event.headers.Authorization;
+  let authParts = authorization.split(' ');
 
-  // parse id from token
-  const token = event.headers.Authorization.split(' ')[1]
-  logger.info("Token", token);
-  // FIXME
+  if (authParts[0].toLowerCase() !== "bearer") {
+    logger.error('Invalid authorization header, missing \'Bearer\'', authorization);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({error: 'Invalid authorization header.'})
+    }
+  }
+
+  const token = authParts[1];
+  logger.info('JWT Token', token);
+
+  if (!!token) {
+    logger.error('Missing JWT', authorization);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({error: 'Invalid or missing auth token.'})
+    }
+  }
+
+  // TODO parse from jwt
   //const userId = parseUserId(token);
   const userId = '001';
+  logger.info('userId', userId);
 
   const result = await getAllTodos(userId);
-
   logger.info("Result", result);
 
   return {
