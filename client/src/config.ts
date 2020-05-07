91ad6f9fa807be0a21d240ea8bb0dcae145495cb
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-07 14:52:42 -0400

    Remove done TODO highlight items

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,7 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = 'xs30ura73c'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'scgrk-dev.auth0.com',            // Auth0 domain
   clientId: '3j6v33XjsS6jIo4FVztlncWuF4CEymtr',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
